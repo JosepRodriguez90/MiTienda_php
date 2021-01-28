a10f3023f23da3ce0550529fc025c74694ab2a42
Josep <65295788+JosepRodriguez90@users.noreply.github.com>
2021-01-28 13:20:30 +0100

    vaciarcesta.php

---
+++ vaciarcesta.php
@@ -0,0 +1,36 @@
+<?php
+
+@session_start();
+$nickLog=$_SESSION["nick_logueado"];
+
+header("Content-Type: text/html;charset=utf-8");
+
+	$servidor="localhost";
+	$usuario="root";
+	$contraseña="usbw";
+  $bd="test";	
+    
+    $c=0;
+
+	$conexion = mysqli_connect($servidor, $usuario, $contraseña, $bd) or die(mysql_error());
+	
+	if (!$conexion)
+	{
+		die("No se ha podido realizar la corrección ERROR:" . mysqli_connect_error() . "<br>");
+	}
+	else
+	{
+		mysqli_set_charset ($conexion, "utf8");
+		//echo "Se ha conectado a la base de datos" . "<br>";
+    }
+
+
+?>
+
+<?php
+
+$instruccion = "DELETE FROM cesta WHERE cliente='$nickLog';
+
+$conexion = mysqli_query($conexion, $instruccion);
+
+?>
